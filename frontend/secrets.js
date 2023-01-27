5e12e7af66ccd0d46344e25d29173d56cfd1d474
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-01-26 20:23:00 -0500

    Add blank secrets

---
+++ frontend/secrets.js
@@ -0,0 +1,10 @@
+const secrets = {
+    clientID: "GOOGLE_AUTH_CLIENTID", // Google Auth
+    clientSecret: "GOOGLE_AUTH_SECRET", // Google Auth
+    platetovin: "PLATE_TO_VIN_API_KEY",
+    XRapidCodeReader: 'CODE_READER_API_KEY',
+    XRapidSalvageTitle: 'SALVAGE_TITLE_API_KEY',
+    NinjasAPI: 'APININJAS_API_KEY'
+}
+
+module.exports = secrets;
