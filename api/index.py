6acd15592279992c931277983cfb9524f8370a51
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-01-26 20:16:33 -0500

    Add YAML Config parser

---
+++ api/index.py
@@ -0,0 +1,18 @@
+from src import create
+from src.config import Config
+from src.models import db
+
+app = create()
+
+
+# Parse YAML config
+config_inst = Config()
+config = config_inst.__dict__
+app.config["SECRET_KEY"] = config["secret_key"]
+for key, value in config["config"].items():
+    app.config[key.upper()] = value
+
+db.init_app(app)
+
+from src.routes import api
+app.register_blueprint(api)
