521ecf1e58b071b616f35f8597d7cd9a0f006637
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-02-20 00:55:49 -0500

    Add authOptions

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,16 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 import secrets from '../../../secrets';
+import type { NextAuthOptions } from 'next-auth';
 
-export default NextAuth({
-    providers: [ 
-        GoogleProvider({ 
+export const authOptions: NextAuthOptions = {
+    secret: "iIbBNi7vujsY3HePIh_yNxOahtjxafLOBsW5Yb-B_wc",
+    providers: [
+        GoogleProvider({
             clientId: secrets.clientID,
             clientSecret: secrets.clientSecret
         }
-)   ]
-})
+    )]
+}
+
+export default NextAuth(authOptions);
